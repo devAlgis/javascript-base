800e3e482679bc4b423670ccfd6e1404f7bac372
devAlgis <71598359+devAlgis@users.noreply.github.com>
2022-05-02 18:07:33 +0300

    Add files via upload

---
+++ 02_syntax/car.js
@@ -0,0 +1,43 @@
+//Задание #3
+const carName = "Toyota";
+let maxCarSpeed = "220 km/h";
+let carOwner = "Alex";
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+
+//Задание #4
+maxCarSpeed = "200 km/h";
+carOwner = "Jhon";
+
+// Первое изменение переменных и вывод
+console.log('У машины изменилась скорость', maxCarSpeed); 
+console.log('У машины изменился владелец', carOwner);
+
+maxCarSpeed = "180 km/h";
+carOwner = "Albert";
+
+// Второе изменение переменных и вывод
+console.log('У машины изменилась скорость', maxCarSpeed); 
+console.log('У машины изменился владелец', carOwner);
+
+// Третье изменение переменных и вывод
+maxCarSpeed = "170 km/h";
+carOwner = "Alice";
+
+console.log('У машины изменилась скорость', maxCarSpeed); 
+console.log('У машины изменился владелец', carOwner);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 02_syntax/index.js
@@ -0,0 +1,19 @@
+// JavaScript код с созданием переменных
+
+/*
+Автор:
+Альгис Хаметов
+*/
+
+const myName = "Algis";
+const mySurname = "Khametov";
+let myFavoriteDrink = "Pepsi";
+let myFavoriteAnimal = "Cat";
+let myFavoriteProgrammingLanguage = "Javascript";
+
+console.log (myName);
+console.log (mySurname);
+console.log (myFavoriteDrink);
+console.log (myFavoriteAnimal);
+console.log (myFavoriteProgrammingLanguage);
+
