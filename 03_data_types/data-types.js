1654675ecaa252856bbcefbe0df2465a988ad776
devAlgis <71598359+devAlgis@users.noreply.github.com>
2022-05-02 21:10:51 +0300

    Add files via upload

---
+++ 03_data_types/data-types.js
@@ -0,0 +1,37 @@
+//Задание №1
+
+//number
+const age = 32;
+console.log('age', age, typeof age);
+
+//string
+const name = 'Spock';
+console.log('name', name, typeof name);
+
+//boolean
+const AmIHungry = false;
+console.log('AmIHungry', AmIHungry, typeof AmIHungry);
+
+//undefined
+const monthSalary = undefined;
+console.log('monthSalary', monthSalary, typeof monthSalary);
+
+//null
+const workExperience = null;
+console.log('workExperience', workExperience, typeof workExperience);
+
+//object
+const character = {
+    name: 'John',
+    lastName: 'Snow',
+    age: 14
+};
+console.log('character', character, typeof character);
+
+//symbol
+const id = Symbol('smbl');
+console.log('id', id, typeof id);
+
+//bigint
+const bigNumber = 100n;
+console.log('bigNumber', bigNumber, typeof bigNumber);
---
+++ 03_data_types/index.js
@@ -0,0 +1,23 @@
+//Задание №2
+let price = 1000;
+price = "2500";
+alert(price);
+
+let product = 'tablet';
+product = 800;
+alert(product);
+
+let availability = false;
+availability = null;
+alert(availability);
+
+console.log(typeof price);
+console.log(typeof product);
+console.log(typeof availability);
+
+
+
+
+
+
+
